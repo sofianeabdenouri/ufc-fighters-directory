5db57e68c0bca7a62cf81f095585347c9a1bac8b
sofianeabdenouri <sofianeabdenouri@gmail.com>
2024-11-21 20:08:03 -0500

    Backend message

--- server.cjs
+++ server.cjs
@@ -11,7 +11,6 @@ app.use(bodyParser.json());
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
-  useUnifiedTopology: true,
   tls: true,
 });
 
@@ -27,54 +26,56 @@ async function connectToDB() {
   }
 }
 
-
 connectToDB();
 
 app.get('/favorites/:userId', async (req, res) => {
-    const { userId } = req.params;
+  const { userId } = req.params;
 
-    try {
-        const userFavorites = await favoritesCollection.findOne({ userId });
-        res.json(userFavorites ? userFavorites.fighterIds : []);
-    } catch (error) {
-        console.error('Error fetching favorites:', error);
-        res.status(500).send('Server error');
-    }
+  try {
+    const userFavorites = await favoritesCollection.findOne({ userId });
+    res.json(userFavorites ? userFavorites.fighterIds : []);
+  } catch (error) {
+    console.error('Error fetching favorites:', error);
+    res.status(500).send('Server error');
+  }
 });
 
 app.post('/favorites/:userId', async (req, res) => {
-    const { userId } = req.params;
-    const { fighterId } = req.body;
+  const { userId } = req.params;
+  const { fighterId } = req.body;
 
-    try {
-        await favoritesCollection.updateOne(
-            { userId },
-            { $addToSet: { fighterIds: fighterId } },
-            { upsert: true }
-        );
-        res.send('Added to favorites');
-    } catch (error) {
-        console.error('Error adding to favorites:', error);
-        res.status(500).send('Server error');
-    }
+  try {
+    await favoritesCollection.updateOne(
+      { userId },
+      { $addToSet: { fighterIds: fighterId } },
+      { upsert: true }
+    );
+    res.send('Added to favorites');
+  } catch (error) {
+    console.error('Error adding to favorites:', error);
+    res.status(500).send('Server error');
+  }
 });
 
 app.delete('/favorites/:userId', async (req, res) => {
-    const { userId } = req.params;
-    const { fighterId } = req.body;
+  const { userId } = req.params;
+  const { fighterId } = req.body;
 
-    try {
-        await favoritesCollection.updateOne(
-            { userId },
-            { $pull: { fighterIds: fighterId } }
-        );
-        res.send('Removed from favorites');
-    } catch (error) {
-        console.error('Error removing from favorites:', error);
-        res.status(500).send('Server error');
-    }
+  try {
+    await favoritesCollection.updateOne(
+      { userId },
+      { $pull: { fighterIds: fighterId } }
+    );
+    res.send('Removed from favorites');
+  } catch (error) {
+    console.error('Error removing from favorites:', error);
+    res.status(500).send('Server error');
+  }
 });
-
+app.get('/', (req, res) => {
+    res.send('Welcome to the UFC Fighters Directory API!');
+  });
+  
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
