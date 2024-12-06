0d88a2c50602b5502e2ee8d9a61260879c4658a7
sofianeabdenouri <sofianeabdenouri@gmail.com>
2024-12-05 20:10:10 -0500

    Production backend

--- vite.config.js
+++ vite.config.js
@@ -2,45 +2,51 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/common/images/**/*',
-          dest: 'common/images',
+export default defineConfig(({ mode }) => {
+  const isDevelopment = mode === 'development';
+
+  return {
+    plugins: [
+      react(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'src/common/images/**/*',
+            dest: 'common/images',
+          },
+          {
+            src: 'src/common/fonts/**/*',
+            dest: 'common/fonts',
+          },
+        ],
+      }),
+    ],
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: './index.html',
+        output: {
+          assetFileNames: 'assets/[name]-[hash][extname]',
         },
-        {
-          src: 'src/common/fonts/**/*',
-          dest: 'common/fonts',
-        },
-      ],
-    }),
-  ],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './index.html',
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
       },
     },
-  },
-  server: mode === 'development' ? {
-    port: 5173,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
+    server: {
+      port: 5173,
+      open: true,
+      proxy: {
+        '/api': {
+          target: isDevelopment
+            ? 'http://localhost:5000' // Development backend
+            : 'https://ufcrec.vercel.app/api', // Production backend
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  } : undefined, // No server proxy in production
-  resolve: {
-    alias: {
-      '@': '/src',
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
-  },
-}));
+  };
+});
