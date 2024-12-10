63493215f229a0af38a03a474796fd22476de304
sofianeabdenouri <sofianeabdenouri@gmail.com>
2024-12-09 19:43:19 -0500

    Index Main App

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = document.getElementById('root');
+if (root) {
+  const reactRoot = ReactDOM.createRoot(root);
+  reactRoot.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
